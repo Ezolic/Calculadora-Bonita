80275d82696f510a38c974d8ef5d52ec410ce814
Ezolic <contatoarthurhenrique@outlook.com>
2023-02-07 20:44:28 -0300

    Modificação onde as divisões e multiplicações são efetuadas primeiro.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 function Calculo(enunciado) {
-  
+
   valor1 = 0
   valor2 = 0
   numeros = []
@@ -13,14 +13,8 @@ function Calculo(enunciado) {
     caractere = enunciado.slice(i, a)
     if (isNaN(caractere)) {
       //letra
-      if (caractere == "x") {
-        operacoes.unshift(caractere);
-      } else if (caractere == "/")
-      {
-        operacoes.unshift(caractere);
-      } else {
-        operacoes.push(caractere);
-      }
+      operacoes.push(caractere);
+
       numeros.push(caracterenovo);
       caracterenovo = "";
     } else {
@@ -33,100 +27,224 @@ function Calculo(enunciado) {
       }
     }
   }
-  let tamanhoonumeros = numeros.length
+  ordem(numeros, operacoes)
+
   let tamanhooperoes = operacoes.length
   for (let i = 0, a = 0, b = 1; i < tamanhooperoes; i++, a++, b++) {
     if (a > 0) {
       if (operacoes[a] == "+") {
         valor2 = parseInt(numeros[b])
         valor1 = valor1 + valor2
-      } else if (operacoes[a] == "-") {
+      } else {
         valor2 = parseInt(numeros[b])
         valor1 = valor1 - valor2
-      } else if (operacoes[a] == "x") {
-        valor2 = parseInt(numeros[b])
-        valor1 = valor1 * valor2
-      } else if (operacoes[a] == "/") {
-        valor2 = parseInt(numeros[b])
-        valor1 = valor1 / valor2
+
       }
     } else {
       if (operacoes[a] == "+") {
         valor1 = parseInt(numeros[a]) + parseInt(numeros[b])
-      } else if (operacoes[a] == "-") {
+      } else  {
         valor1 = parseInt(numeros[a]) - parseInt(numeros[b])
-      } else if (operacoes[a] == "x") {
-        valor1 = parseInt(numeros[a]) * parseInt(numeros[b])
-      } else if (operacoes[a] == "/") {
-        valor1 = parseInt(numeros[a]) / parseInt(numeros[b])
       }
     }
   }
-
-  console.log(numeros)
-  console.log(operacoes)
-
-
   return valor1;
-}
 
-function atualizaNumero(num) {
-  valorantigo = document.getElementById("numero").value;
 
-  if(valorantigo == 0) {
-    document.getElementById("numero").value = num;
-  } else {
-    valornovo = valorantigo + num;
+  }
 
-    document.getElementById("numero").value = valornovo;
-  } 
-}
 
-function limpar() {
-  document.getElementById("numero").value = "0";
-}
+  function atualizaNumero(num) {
+    valorantigo = document.getElementById("numero").value;
 
-function limparCaractere() {
-  valor = document.getElementById("numero").value;
+    if (valorantigo == 0) {
+      document.getElementById("numero").value = num;
+    } else {
+      valornovo = valorantigo + num;
 
-  if (valor.length == 1) {
+      document.getElementById("numero").value = valornovo;
+    }
+  }
+
+  function limpar() {
     document.getElementById("numero").value = "0";
-  }  else {
-    valornovo = valor.length - 1;
+  }
 
-    document.getElementById("numero").value = valor.slice(0, valornovo);
+  function limparCaractere() {
+    valor = document.getElementById("numero").value;
+
+    if (valor.length == 1) {
+      document.getElementById("numero").value = "0";
+    } else {
+      valornovo = valor.length - 1;
+
+      document.getElementById("numero").value = valor.slice(0, valornovo);
+
+    }
+  }
+
+  function atualizaOperacao(operacao) {
+
+    enunciado = document.getElementById("numero").value;
+
+    caractere = enunciado.slice(enunciado.length - 1, enunciado.length)
+
+    if (isNaN(caractere) || (enunciado.length == 1 && caractere == 0)) {
+      return false;
+    } else {
+      enunciado = enunciado + operacao;
+      document.getElementById("numero").value = enunciado;
+
+    }
+  }
+
+  function realizarConta() {
+
+    enunciado = document.getElementById("numero").value;
+
+    if (isNaN(enunciado.slice(enunciado.length - 1, enunciado.length))) {
+      return false;
+    } else {
+      Calculo(enunciado);
+
+      enunciado = document.getElementById("numero").value = valor1;
+    }
+  }
+
+
+
+  function ordem(numeros, operacoes) {
+
+    operadores = operacoes.filter(word => word == 'x' || word == '/');
+
+    console.log(operadores.length)
+
+
+    for (let i = 0; i < operadores.length; i++) {
+
+      if (numeros.length == 1) {
+        return numeros, operacoes
+      }
+
+      if (operacoes.filter(x => x == 'x').length) {
+        posicao = operacoes.indexOf('x')
+
+        console.log("Caiu divisão " + i)
+
+
+        if (posicao > 0) {
+          num = numeros[posicao] * numeros[posicao + 1];
+
+          operacoes.splice(posicao, 1)
+
+          numeros.splice(posicao, posicao + 1, num);
+        } else {
+          num = numeros[posicao] * numeros[posicao + 1];
+
+          operacoes.splice(posicao, 1)
+
+          numeros.splice(posicao, posicao + 2, num);
+
+        }
+
+        console.log(numeros);
+        console.log(operacoes);
+
+
+      } else if (operacoes.filter(x => x == '/').length) {
+        posicao = operacoes.indexOf('/')
+
+        console.log("Caiu multiplicação " + i)
+
+
+        if (posicao > 0) {
+          num = numeros[posicao] / numeros[posicao + 1];
+
+          operacoes.splice(posicao, 1)
+
+          numeros.splice(posicao, posicao + 1, num);
+        } else {
+          num = numeros[posicao] / numeros[posicao + 1];
+
+          operacoes.splice(posicao, 1)
+
+          numeros.splice(posicao, posicao + 2, num);
+        }
+
+        console.log(numeros);
+        console.log(operacoes);
+
+      } else {
+        return numeros, operacoes
+      }
+    }
 
   }
-}
 
-function atualizaOperacao(operacao) {
+/*
+numeros = [10,5,10,10,10,10,10,10]
+operacoes = ['x','x','x','x','x','x','x']
 
-  enunciado = document.getElementById("numero").value;
+operadores = operacoes.filter(word => word == 'x' || word == '/');
 
-  caractere = enunciado.slice(enunciado.length - 1, enunciado.length)
+console.log(operadores.length)
 
-  if(isNaN(caractere) || enunciado.length == 1) {
-    return false;
-  } else {
-    enunciado = enunciado + operacao;
-    document.getElementById("numero").value = enunciado;
+
+  for (let i = 0; i < operadores.length; i++) {
+
+
+
+    if (operacoes.filter(x => x == 'x').length) {
+      posicao = operacoes.indexOf('x')
+
+      console.log("Caiu divisão " + i)
+
+
+      if (posicao > 0) {
+        num = numeros[posicao] * numeros[posicao + 1];
+
+        operacoes.splice(posicao, 1)
+
+        numeros.splice(posicao, posicao + 1, num);
+      } else {
+        num = numeros[posicao] * numeros[posicao + 1];
+
+        operacoes.splice(posicao, 1)
+
+        numeros.splice(posicao, posicao + 2, num);
+        
+      }
+
+      console.log(numeros);
+      console.log(operacoes);
+
+
+    } else if (operacoes.filter(x => x == '/').length) {
+      posicao = operacoes.indexOf('/')
+
+      console.log("Caiu multiplicação " + i)
+
+
+      if (posicao > 0) {
+        num = numeros[posicao] / numeros[posicao + 1];
+
+        operacoes.splice(posicao, 1)
+
+        numeros.splice(posicao, posicao + 1, num);
+      } else {
+        num = numeros[posicao] / numeros[posicao + 1];
+
+        operacoes.splice(posicao, 1)
+
+        numeros.splice(posicao, posicao + 2, num);
+      }
+
+      console.log(numeros);
+      console.log(operacoes);
+
+    } else { }
   }
-}
 
-function realizarConta() {
-
-  enunciado = document.getElementById("numero").value;
-
-  if(isNaN(enunciado.slice(enunciado.length - 1, enunciado.length))) {
-    return false;
-  } else {
-    Calculo(enunciado);
-
-    enunciado = document.getElementById("numero").value = valor1;
-  }
-}
-
-
-
+*/
 
 
